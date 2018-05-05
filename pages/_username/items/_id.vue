ecb9f425625b829680bf5037de11c53bbe665f0c
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-06 00:38:27 +0900

    Add Post Details

--- pages/_username/items/_id.vue
+++ pages/_username/items/_id.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    <h1>Post Details</h1>
+  <div class="container mx-auto">
+    <div class="flex mb-4 bg-grey-lighter">
+      <div class="w-4/5 text-grey-darker bg-grey-light px-4 py-2 m-2">
+        <!-- Markdown -->
+        <p>
+          Lorem Ipsum asdflkajsdflkjasdflkjasdlfkjasdlfkjasdflkjasd
+        </p>
+
+        <hr>
+        <!-- Comments -->
+        <form action="">
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </form>
+        <ul>
+          <li>Comment 1</li>
+          <li>Comment 2</li>
+          <li>Comment 3</li>
+        </ul>
+      </div>
+      <div class="w-1/5 text-grey-darker bg-grey-light px-4 py-2 m-2">
+        <ul class="sticky pin-y pin-r">
+          <li>Title 1</li>
+          <li>Title 2</li>
+          <li>Title 3</li>
+          <li>Title 4</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
