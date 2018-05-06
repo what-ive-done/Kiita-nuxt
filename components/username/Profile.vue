daa96d78ad39b71a46d02157a4a21ae57ed40ffd
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-06 11:04:11 +0900

    Add Profile / TopMenu for username Layout

---
+++ components/username/Profile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+    <div class="sm:flex sm:flex-col sm:items-center px-6 py-4">
+      <img
+        class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+        src="https://qiita-image-store.s3.amazonaws.com/0/156279/profile-images/1525094577"
+        alt="changjoo-park"
+      >
+      <div class="text-center sm:text-left sm:flex-grow">
+        <div class="mb-4">
+          <p class="text-xl leading-tight">ChangJoo Park</p>
+          <p class="text-sm leading-tight text-grey-dark">@<span>changjoo-park</span></p>
+          <div class="border-t border-purple mt-4">
+            <p class="text-sm leading-tight text-grey-dark">pcjpcj2@gmail.com</p>
+            <p class="text-sm leading-tight text-grey-dark">Seoul, Korea</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <nuxt-link
+          class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple no-underline hover:bg-purple hover:text-white"
+          :to="{ name: 'settings-account' }"
+        >
+          Edit Profile
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/username/TopMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li>
+      <nuxt-link
+        :to="{ name: 'username', params: { username: 'changjoo-park' } }"
+      >
+      Items
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link
+        :to="{ name: 'username-contributes', params: { username: 'changjoo-park' } }"
+      >
+      Contributes
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link
+        :to="{ name: 'username-followers', params: { username: 'changjoo-park' } }"
+      >
+        Followers
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
