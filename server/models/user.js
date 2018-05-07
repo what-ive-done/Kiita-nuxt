bd7576df800e4f3531b9cc6f80a657183332ff71
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-07 14:02:46 +0900

    Add relationship user and comments

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
     User.hasMnay(models.Item)
+    User.hasMnay(models.Comment)
   };
   return User;
 };
