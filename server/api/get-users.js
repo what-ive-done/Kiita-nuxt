5d83c2ca03cbb050b27e79c005738a636e9cfb72
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 00:00:45 +0900

    Update fetch User API

--- server/api/get-users.js
+++ server/api/get-users.js
@@ -3,8 +3,14 @@ const router = Router()
 const db = require('../models/index.js')
 
 router.get('/users', async (req, res, next) => {
-  const users = await db.User.findAll()
-  res.json(users)
+  try {
+    console.log(db)
+    const users = await db.User.findAll()
+    res.json(users)
+  } catch (e) {
+    console.log(e)
+    res.json({ message: e })
+  }
 })
 
 module.exports = router
