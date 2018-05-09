f0b0449345ea8337409815f48162d9e79788da85
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 23:01:53 +0900

    Add get tag API

--- server/api/get-tag.js
+++ server/api/get-tag.js
@@ -1,9 +1,15 @@
 const { Router } = require('express')
-
 const router = Router()
+const db = require('../models')
 
-router.get('/tags/:tag_id', (req, res, next) => {
-  console.log('api one User')
+router.get('/tags/:name', async (req, res, next) => {
+  const { name } = req.params
+  try {
+    const tag = await db.Tag.findOne({ where: { name } })
+    res.status(200).json(tag)
+  } catch (error) {
+    res.json({ message: error })
+  }
   res.sendStatus(200)
 })
 
