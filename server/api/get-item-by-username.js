464579f1d2f7d6b3d1461d4df3576e991973e42e
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 22:46:06 +0900

    Add Get Item by Username and Item Id

--- server/api/get-item-by-username.js
+++ server/api/get-item-by-username.js
@@ -1,10 +1,18 @@
 const { Router } = require('express')
-
 const router = Router()
+const db = require('../models/index.js')
 
-router.get('/users/:user_id/item/:item_id', (req, res, next) => {
-  console.log('api one User')
-  res.sendStatus(200)
+router.get('/users/:username/items/:item_id', async (req, res, next) => {
+  console.log('get item by username')
+  const { username, item_id:itemId } = req.params
+  console.log('username => ', username)
+  try {
+    const user = await db.User.findOne({ where: { username }})
+    const item = await db.Item.findOne({ where: { UserId: user.id, id: itemId }})
+    return res.status(200).json(item)
+  } catch (error) {
+    res.json({ message: error })
+  }
 })
 
 module.exports = router
