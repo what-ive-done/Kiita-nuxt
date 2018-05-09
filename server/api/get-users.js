b75deee1f7cf7c03c7d9e4ec0c0918d3f50c16c6
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 22:00:37 +0900

    get users by offset limit

--- server/api/get-users.js
+++ server/api/get-users.js
@@ -4,12 +4,15 @@ const db = require('../models/index.js')
 
 router.get('/users', async (req, res, next) => {
   try {
-    console.log(db)
-    const users = await db.User.findAll()
-    res.json(users)
+    const { offset = 0, limit = 30 } = req.query
+    const users = await db.User.findAll({
+      offset,
+      limit
+    })
+    return res.json(users)
   } catch (e) {
     console.log(e)
-    res.json({ message: e })
+    return res.json({ message: e })
   }
 })
 
