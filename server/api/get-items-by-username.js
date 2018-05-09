cc159ca967e47609249840ab8501e3bcaf53a2c0
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 22:46:23 +0900

    Add Get Items by Username

--- server/api/get-items-by-username.js
+++ server/api/get-items-by-username.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
-
 const router = Router()
+const db = require('../models/index.js')
 
-router.get('/users/:user_id/items', (req, res, next) => {
-  console.log('api one User')
+router.get('/users/:username/items', async (req, res, next) => {
+  const { username, itemId } = req.params
+  try {
+    const user = await db.User.findOne({ where: { username } })
+    const items = await db.Item.findAll({ where: { UserId: user.id }})
+    res.json(items)
+  } catch (error) {
+    res.json({ message: error })
+  }
   res.sendStatus(200)
 })
 
