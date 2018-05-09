de06aa52d14ece7eb95c4869795382281ffcbcc3
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-09 22:12:31 +0900

    Update get-user

--- server/api/get-user.js
+++ server/api/get-user.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
-
+const db = require('../models/index.js')
 const router = Router()
 
-router.get('/users/:user_id', (req, res, next) => {
-  console.log('api one User')
+router.get('/users/:username', async (req, res, next) => {
+  console.log('get user')
+  const { username } = req.params
+  try {
+    const user = await db.User.findOne({
+      where: { username }
+    })
+
+    if (user === null) {
+      return res.status(404).json({ message: "Not Found" })
+    }
+    return res.json(user)
+  } catch (error) {
+    console.error(error)
+    return res.json({ message: e })
+  }
   res.sendStatus(200)
 })
 
