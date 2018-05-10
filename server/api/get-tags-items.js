412faa898d7e07d2833057f3ed631a047b091da0
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-10 22:43:13 +0900

    Implement items of specific tag

--- server/api/get-tags-items.js
+++ server/api/get-tags-items.js
@@ -1,9 +1,21 @@
 const { Router } = require('express')
-
 const router = Router()
+const db = require('../models')
 
-router.post('/tags/:tag_id/items', (req, res, next) => {
-  console.log('api one User')
+router.get('/tags/:tag_id/items', async (req, res, next) => {
+  try {
+    const tag = await db.Tag.findAll({
+      include: [
+        {
+          model: db.Item,
+          through: { attributes: [] }
+        }
+      ]
+    })
+    return res.json(tag)
+  } catch (error) {
+    res.json({ message: error })
+  }
   res.sendStatus(200)
 })
 
