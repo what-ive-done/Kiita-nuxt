783905523990fc7c03065fbdab315c265b6f5a60
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-10 23:37:47 +0900

    Implements sign in API

--- server/api/sign-in.js
+++ server/api/sign-in.js
@@ -1,10 +1,19 @@
 const { Router } = require('express')
-
+const jwt = require('jsonwebtoken');
 const router = Router()
+const db = require('../models')
 
-router.post('/sign-in', (req, res, next) => {
-  console.log('api post')
-  res.sendStatus(200)
+router.post('/auth/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await db.User.findOne({
+      where: { username, password }
+    })
+    let accessToken = jwt.sign({ id: user.id, username: user.username }, 'SECRET', { expiresIn: 129600 });
+    res.json({ accessToken })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
