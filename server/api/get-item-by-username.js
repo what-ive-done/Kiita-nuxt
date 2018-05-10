785fb23a5abaef17d33f99377a24d32eb613c18b
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-10 22:42:54 +0900

    Formatting

--- server/api/get-item-by-username.js
+++ server/api/get-item-by-username.js
@@ -7,7 +7,7 @@ router.get('/users/:username/items/:item_id', async (req, res, next) => {
   const { username, item_id:itemId } = req.params
   console.log('username => ', username)
   try {
-    const user = await db.User.findOne({ where: { username }})
+    const user = await db.User.findOne({ where: { username } })
     const item = await db.Item.findOne({ where: { UserId: user.id, id: itemId }})
     return res.status(200).json(item)
   } catch (error) {
