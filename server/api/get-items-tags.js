4e2490c967237f10384f4a274abd3064abdb56d8
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-10 23:10:49 +0900

    Get tags by Item Id

--- server/api/get-items-tags.js
+++ server/api/get-items-tags.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
-
 const router = Router()
+const db = require('../models')
 
-router.post('/items/:item_id/tags', (req, res, next) => {
-  console.log('api one User')
+router.get('/items/:id/tags', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const item = await db.Item.findById(id, {
+      include: [
+        {
+          model: db.Tag,
+          through: { attributes: [] }
+        }
+      ]
+    })
+    return res.json(item)
+  } catch (error) {
+    console.log(error)
+    res.json({ message: error })
+  }
   res.sendStatus(200)
 })
 
