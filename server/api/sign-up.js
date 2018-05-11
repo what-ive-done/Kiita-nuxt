e77210b6f62c2b1592dbe12583437c657c603ded
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-11 22:37:01 +0900

    Implements sign up

--- server/api/sign-up.js
+++ server/api/sign-up.js
@@ -1,10 +1,17 @@
 const { Router } = require('express')
-
+const db = require('../models')
 const router = Router()
 
-router.post('/sign-up', (req, res, next) => {
-  console.log('api sign-up post')
-  res.sendStatus(200)
+router.post('/sign-up', async (req, res, next) => {
+  try {
+    const { email, username, password } = req.body
+    const savedUser = await db.User.create({
+      email, username, password
+    })
+    res.status(200).json(savedUser)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
