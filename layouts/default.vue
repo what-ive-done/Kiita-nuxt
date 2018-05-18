2a4d00f8c84b87a9567002c4c1435706de227ba3
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-18 23:39:59 +0900

    Add global style to layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,5 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~/assets/style.scss';
+
 </style>
--- layouts/username.vue
+++ layouts/username.vue
@@ -30,5 +30,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~/assets/style.scss';
+
 </style>
