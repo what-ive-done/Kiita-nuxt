3a4b414f929dc391ed010af04880b515ae6df6d7
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-19 00:19:20 +0900

    WIP : Update submenu inside items

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
-    <ul>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username', params: { username: profileUser.username } }"
-        >
-          Recent Post
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username-comments', params: { username: profileUser.username } }"
-        >
-          Comments
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username-stocked', params: { username: profileUser.username } }"
-        >
-          Stocked
-        </nuxt-link>
-      </li>
-    </ul>
+    <div class="pure-menu pure-menu-horizontal">
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username', params: { username: profileUser.username } }"
+          >
+            Recent Post
+          </nuxt-link>
+        </li>
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username-comments', params: { username: profileUser.username } }"
+          >
+            Comments
+          </nuxt-link>
+        </li>
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username-stocked', params: { username: profileUser.username } }"
+          >
+            Stocked
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
     Recent Post
   </div>
 </template>
--- pages/_username/stocked.vue
+++ pages/_username/stocked.vue
@@ -1,36 +1,47 @@
 <template>
   <div>
-    <ul>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username', params: { username: 'changjoo-park' } }"
-        >
-          Recent Post
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username-comments', params: { username: 'changjoo-park' } }"
-        >
-          Comments
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link
-          :to="{ name: 'username-stocked', params: { username: 'changjoo-park' } }"
-        >
-          Stocked
-        </nuxt-link>
-      </li>
-    </ul>
-    Stocked Items
+    <div class="pure-menu pure-menu-horizontal">
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username', params: { username: profileUser.username } }"
+          >
+            Recent Post
+          </nuxt-link>
+        </li>
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username-comments', params: { username: profileUser.username } }"
+          >
+            Comments
+          </nuxt-link>
+        </li>
+        <li class="pure-menu-item">
+          <nuxt-link
+            class="pure-menu-link"
+            :to="{ name: 'username-stocked', params: { username: profileUser.username } }"
+          >
+            Stocked
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+    Stocked
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   layout: 'username',
-  components: {
+  fetch({ store, params }) {
+    store.dispatch('getProfileUser', params['username'])
+  },
+  computed: {
+    ...mapGetters(['profileUser'])
   }
 }
 </script>
