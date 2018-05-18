52153e9ec2d806fb3757dc5ef6959d845761802a
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-19 00:18:35 +0900

    Update Username top menu

--- components/username/TopMenu.vue
+++ components/username/TopMenu.vue
@@ -1,27 +1,32 @@
 <template>
-  <ul>
-    <li>
-      <nuxt-link
-        :to="{ name: 'username', params: { username: 'changjoo-park' } }"
-      >
-      Items
-      </nuxt-link>
-    </li>
-    <li>
-      <nuxt-link
-        :to="{ name: 'username-contributes', params: { username: 'changjoo-park' } }"
-      >
-      Contributes
-      </nuxt-link>
-    </li>
-    <li>
-      <nuxt-link
-        :to="{ name: 'username-followers', params: { username: 'changjoo-park' } }"
-      >
-        Followers
-      </nuxt-link>
-    </li>
-  </ul>
+  <div class="pure-menu pure-menu-horizontal">
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username', params: { username: 'changjoo-park' } }"
+        >
+        Items
+        </nuxt-link>
+      </li>
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username-contributes', params: { username: 'changjoo-park' } }"
+        >
+        Contributes
+        </nuxt-link>
+      </li>
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username-followers', params: { username: 'changjoo-park' } }"
+        >
+          Followers
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
