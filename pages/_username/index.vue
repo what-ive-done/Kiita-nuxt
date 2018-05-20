4fca7c5b6da5cf0a2c05847859741ef4de84a010
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-20 19:34:51 +0900

    Update username page

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -28,17 +28,36 @@
         </li>
       </ul>
     </div>
-    Recent Post
+    <div v-for="post in posts" :key="post.id">
+      <nuxt-link
+        :to="{ name: 'username-items-id', params: { username: profileUser.username, id: post.id }}"
+      >{{ post.title }}</nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import { mapGetters } from 'vuex'
 
 export default {
   layout: 'username',
   fetch({ store, params }) {
     store.dispatch('getProfileUser', params['username'])
+  },
+  async asyncData (context) {
+    try {
+      const { username } = context.route.params
+      const { data: posts } = await axios({ method: 'GET', url: `http://localhost:8080/api/users/${username}/items` })
+      return { posts }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  data () {
+    return {
+      posts: []
+    }
   },
   computed: {
     ...mapGetters(['profileUser'])
