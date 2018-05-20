8fe8e9f8454fd889ec2c4a488aff7aa2c8c6b4f4
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-21 00:11:39 +0900

    Update comments listing

--- pages/_username/comments.vue
+++ pages/_username/comments.vue
@@ -28,17 +28,29 @@
         </li>
       </ul>
     </div>
-    Comment
+    <div>
+      <div v-for="comment in comments" :key="comment.id">
+        <small>{{ comment.Item.title }}</small> <br>
+        {{ comment.body }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import axios from 'axios'
 
 export default {
   layout: 'username',
-  fetch({ store, params }) {
-    store.dispatch('getProfileUser', params['username'])
+  async asyncData (context) {
+    try {
+      const { username } = context.route.params
+      const { data } = await axios({ method: 'GET', url: `http://localhost:8080/api/users/${username}/comments` })
+      return { comments: data.Comments }
+    } catch (error) {
+      console.error(error)
+    }
   },
   computed: {
     ...mapGetters(['profileUser'])
