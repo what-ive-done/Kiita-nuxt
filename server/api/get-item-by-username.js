482aa5a44c17ab2f22fe2c69e20a8481aaa97597
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-20 19:35:08 +0900

    update api : get-item-by-username include comments and comment-user

--- server/api/get-item-by-username.js
+++ server/api/get-item-by-username.js
@@ -3,12 +3,22 @@ const router = Router()
 const db = require('../models/index.js')
 
 router.get('/users/:username/items/:item_id', async (req, res, next) => {
-  console.log('get item by username')
   const { username, item_id:itemId } = req.params
-  console.log('username => ', username)
   try {
     const user = await db.User.findOne({ where: { username } })
-    const item = await db.Item.findOne({ where: { UserId: user.id, id: itemId }})
+    const item = await db.Item.findOne({
+      where: { UserId: user.id, id: itemId },
+      include: [
+        {
+          model: db.Comment,
+          include: [
+            {
+              model: db.User
+            }
+          ]
+        }
+      ]
+    })
     return res.status(200).json(item)
   } catch (error) {
     res.json({ message: error })
