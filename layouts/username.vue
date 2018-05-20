f75beb89fdbf29bb5eb00ad520aab38417010a63
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-21 00:01:01 +0900

    Passing through user info from page

--- layouts/username.vue
+++ layouts/username.vue
@@ -3,7 +3,10 @@
     <navigation />
     <div class="pure-g">
       <div class="pure-u-6-24">
-        <profile />
+        <profile
+          :user="profileUser"
+          :is-editable="$auth.user && ($auth.user.username === profileUser.username)"
+        />
       </div>
       <div class="pure-u-18-24">
         <top-menu />
@@ -14,17 +17,26 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Navigation from '~/components/Navigation.vue'
 import Profile from '~/components/username/Profile.vue'
 import TopMenu from '~/components/username/TopMenu.vue'
-
 export default {
   components: {
     Navigation,
     Profile,
     TopMenu
   },
-
+  computed: {
+    ...mapGetters(['profileUser']),
+    isEditable () {
+      try {
+        return $auth.user && ($auth.user.username === profileUser.username)
+      } catch (error) {
+        return false
+      }
+    }
+  }
 }
 </script>
 
