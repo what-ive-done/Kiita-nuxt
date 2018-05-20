5419f886b238844133148ae82e9fa87fe44d1936
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-20 19:05:16 +0900

    Add first test

---
+++ components/Logo.test.js
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils'
+import Logo from './Logo.vue'
+
+describe('로고 컴포넌트', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Logo)
+  })
+  test('로고 컴포넌트는 Vue 인스턴스이다.', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
