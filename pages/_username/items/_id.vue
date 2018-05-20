8974655123c0538cd40ef057fa5c07a28312d3a5
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-20 19:35:21 +0900

    Update post route

--- pages/_username/items/_id.vue
+++ pages/_username/items/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto">
-    <div class="flex mb-4 bg-grey-lighter">
-      <div class="w-4/5 text-grey-darker bg-grey-light px-4 py-2 m-2">
+    <div class="">
+      <div class="">
         <!-- Markdown -->
         <h1>
           {{ post.title }}
@@ -12,20 +12,14 @@
         <hr>
         <!-- Comments -->
         <ul>
-          <li v-for="comment in post.comments" :key="comment.id">
-            {{ comment.body }}
+          <li v-for="comment in post.Comments" :key="comment.id">
+            <nuxt-link
+              :to="{ name: 'username', params: { username: comment.User.username } }"
+            >
+              {{ comment.User.username }}
+            </nuxt-link>
+             - {{ comment.body }}
           </li>
-        </ul>
-        <form action="">
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </form>
-      </div>
-      <div class="w-1/5 text-grey-darker bg-grey-light px-4 py-2 m-2">
-        <ul class="sticky pin-y pin-r">
-          <li>Title 1</li>
-          <li>Title 2</li>
-          <li>Title 3</li>
-          <li>Title 4</li>
         </ul>
       </div>
     </div>
@@ -36,6 +30,16 @@
 import axios from 'axios'
 
 export default {
+  async asyncData (context) {
+    try {
+      const { username, id } = context.route.params
+      console.log('id => ', id)
+      const { data: post } = await axios({ method: 'GET', url: `http://localhost:8080/api/users/${username}/items/${id}` })
+      return { post }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 </script>
 
