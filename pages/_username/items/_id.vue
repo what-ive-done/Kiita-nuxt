b118c29ef02e1b2749b39ebf9b247913280d1102
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-20 22:43:10 +0900

    Update Post show design

--- pages/_username/items/_id.vue
+++ pages/_username/items/_id.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container mx-auto">
-    <div class="">
-      <div class="">
+    <div class="pure-g">
+
+    </div>
+    <div class="pure-u-3-5 content">
         <!-- Markdown -->
         <h1>
           {{ post.title }}
@@ -10,18 +12,28 @@
           {{ post.body }}
         </p>
         <hr>
-        <!-- Comments -->
-        <ul>
-          <li v-for="comment in post.Comments" :key="comment.id">
-            <nuxt-link
-              :to="{ name: 'username', params: { username: comment.User.username } }"
-            >
-              {{ comment.User.username }}
-            </nuxt-link>
-             - {{ comment.body }}
-          </li>
-        </ul>
+    </div>
+    <div class="pure-u-2-5 table-of-content">
+      <div>
+        <img src="http://lorempicsum.com/futurama/350/200/1" alt="ad-1" width="100%">
       </div>
+      <ul>
+        <li>Table Of Content</li>
+      </ul>
+    </div>
+
+    <div class="pure-u-1">
+      <!-- Comments -->
+      <ul>
+        <li v-for="comment in post.Comments" :key="comment.id">
+          <nuxt-link
+            :to="{ name: 'username', params: { username: comment.User.username } }"
+          >
+            {{ comment.User.username }}
+          </nuxt-link>
+            - {{ comment.body }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -44,5 +56,13 @@ export default {
 </script>
 
 <style>
+.content {
+  box-sizing: border-box;
+  padding: 1em;
+}
 
+.table-of-content {
+  box-sizing: border-box;
+  padding: 1em;
+}
 </style>
