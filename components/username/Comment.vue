ca15b9ce5635edd814d0ca9220885a963ba57543
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-21 23:35:49 +0900

    Add username's comment component

---
+++ components/username/Comment.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="comment && item">
+    <div>
+      <nuxt-link
+        class="comment-item-title"
+        :to="{
+          name: 'username-items-id',
+          params: {
+            username: item.User.username,
+            id: item.id
+          }
+        }"
+      >
+        {{ item.title }}
+      </nuxt-link>
+      <div class="comment-created-at">{{ comment.createdAt }}</div>
+    </div>
+    <div class="comment-body">{{ comment.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    },
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
