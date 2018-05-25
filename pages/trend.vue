bdfd08e1d6d6945dcc0312e26658f1d57b5dd07f
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-25 22:02:08 +0900

    Render item component for posts

--- pages/trend.vue
+++ pages/trend.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container">
-    {{ posts }}
+    <item v-for="post in posts" :key="post.id" :item="post"/>
   </div>
 </template>
 
 <script>
+import Item from '~/components/Item.vue'
 import { mapGetters } from 'vuex'
 
 export default {
+  components: {
+    Item
+  },
   fetch({ store }) {
     store.dispatch('getPosts')
   },
