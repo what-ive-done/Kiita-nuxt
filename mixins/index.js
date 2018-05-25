0b521a77480bf138a9892e2c4a7bf6f515ade70c
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-25 22:01:46 +0900

    Add mixins for ListItem

--- mixins/index.js
+++ mixins/index.js
@@ -1,3 +1,6 @@
+import { distanceInWords, differenceInCalendarDays, format } from 'date-fns'
+import koLocale from 'date-fns/locale/ko'
+
 const dateformat = {
   filters: {
     formattedDate (value) {
@@ -6,6 +9,28 @@ const dateformat = {
     }
   }
 }
+
+const listItem = {
+  filters: {
+    daysAgo (value) {
+      const now = new Date()
+      const dayDiff = differenceInCalendarDays(now, value)
+      if (dayDiff > 20) {
+        return format(value, 'YYYY.MM.DD')
+      }
+
+      const locale = { locale: koLocale }
+      const days = distanceInWords(value, now, locale)
+      return `${days} 전`
+    },
+
+    stripedTitle (value) {
+      return value.slice(0, 20) || ''
+    }
+  }
+}
+
 export default {
-  dateformat
+  dateformat,
+  listItem
 }
