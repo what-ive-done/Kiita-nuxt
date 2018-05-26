5feb1efb68de85ab9dfd3ca66979ae846fdd1612
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-27 03:40:55 +0900

    Add tag component

---
+++ components/Tag.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="tag">{{ tag.name }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.tag {
+  background-color: grey;
+  color: white;
+  margin-right: 10px;
+}
+</style>
