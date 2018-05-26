ade78782400b074d66ca3bc8b30d23369252fef7
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-27 03:41:07 +0900

    Add Item Details components

--- components/item/Details.vue
+++ components/item/Details.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <h1 class="item-title">{{ item.title }}</h1>
+    <tag
+      v-for="tag in item.Tags"
+      :key="tag.id"
+      :tag="tag"
+    />
     <p class="item-details">{{ item.body }}</p>
   </div>
 </template>
 
 <script>
+import Tag from '@/components/Tag'
+
 export default {
   props: {
     item: {
       type: Object,
       required: true
     }
+  },
+  components: {
+    Tag
   }
 }
 </script>
