827a22e6dbef4b31e05370d8eefc2cd897dfa2fb
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-26 22:49:35 +0900

    WIP show tag name and items counts

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="container mx-auto">
-    <div class="flex mb-4 bg-grey-lighter">
-      <div class="w-1/4 md:w-1/4 sm:w-full text-grey-darker text-center bg-grey-light px-4 py-2 m-2">
-      </div>
-      <div class="w-3/4 md:w-3/4 sm:w-full text-grey-darker bg-grey-light px-4 py-2 m-2">
-      </div>
-    </div>
+  <div class="container">
+    <h1>Hello</h1>
+    <span v-for="tag in tags" :key="tag.id">
+      {{ tag.name }} - {{ getItemsCount(tag.Items )}}
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  async asyncData ({ app }) {
+    try {
+      const data = await app.$axios.$get('http://localhost:8080/api/tags')
+      return { tags: data }
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  methods: {
+    getItemsCount (items) {
+      return items.length || 0
+    }
+  }
 }
 </script>
 
