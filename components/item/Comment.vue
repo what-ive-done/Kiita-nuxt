a70c0728f0fc892b25dfa0d5e33e4f35f229c65e
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-27 03:47:02 +0900

    Add Comment component for Item

--- components/item/Comment.vue
+++ components/item/Comment.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
-    <nuxt-link
-      :to="{ name: 'username', params: { username: comment.User.username } }"
-    >
-      {{ comment.User.username }}
-    </nuxt-link>
-      - {{ comment.body }}
+    <div class="comment-avatar">
+      <gravatar :email="comment.User.email" />
+    </div>
+    <div>
+      <nuxt-link
+        :to="{ name: 'username', params: { username: comment.User.username } }"
+      >
+        {{ comment.User.username }}
+      </nuxt-link>
+        - {{ comment.createdAt }}
+    </div>
+    <div>
+      {{ comment.body }}
+    </div>
   </div>
 </template>
 
 <script>
+import Gravatar from '@/components/Gravatar'
+
 export default {
   props: {
     comment: {
       type: Object,
       required: true
     }
+  },
+  components: {
+    Gravatar
   }
 }
 </script>
 
 <style>
-
+.comment-avatar {
+  width: 50px;
+}
 </style>
