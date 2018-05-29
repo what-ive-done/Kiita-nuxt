b00d6eea203b3d89acec4f15fa48f9d69b16c06a
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-29 23:16:11 +0900

    Add SubMenu component for username route

---
+++ components/username/SubMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="pure-menu pure-menu-horizontal">
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username', params: { username: profileUser.username } }"
+        >
+          Recent Post
+        </nuxt-link>
+      </li>
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username-comments', params: { username: profileUser.username } }"
+        >
+          Comments
+        </nuxt-link>
+      </li>
+      <li class="pure-menu-item">
+        <nuxt-link
+          class="pure-menu-link"
+          :to="{ name: 'username-stocked', params: { username: profileUser.username } }"
+        >
+          Stocked
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['profileUser'])
+  }
+}
+</script>
+
+<style>
+
+</style>
